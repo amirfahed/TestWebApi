358d61a9f65b351154fe0af96ea8784a96815e85
Amir <a.fahed@atomyze.ru>
2021-10-31 12:15:31 +0300

    first commit

---
+++ Model/TodoContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TestWebApi.Model
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options){
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set;}
+    }
+}
