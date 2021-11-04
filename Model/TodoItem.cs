a171684eb355b4062fdd74807c148da9fec3505a
Amir <a.fahed@atomyze.ru>
2021-11-04 22:02:29 +0300

    add service

--- Model/TodoItem.cs
+++ Model/TodoItem.cs
@@ -4,6 +4,7 @@ namespace TestWebApi.Model
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public bool IsComplete { get; set;}
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
     }
 }
